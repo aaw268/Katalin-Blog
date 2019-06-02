b2ea3918bbca46d881c39c35daeb65ac4423ecf3
Anthony Williams <aaw268@nau.edu>
2019-06-01 18:00:48 -0700

    basic layout, start working on gallery or home page

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.photo-scroll').slick({
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+  });
+});
